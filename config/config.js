d03b7e290bacc3ae550f4058983eceb876ad0e20
khurram17452 <khurram17452@gmail.com>
2023-05-16 02:03:58 +0500

    added mongodb string

--- config/config.js
+++ config/config.js
@@ -1,10 +1,18 @@
 const mongoose = require("mongoose");
 
 
-const connection = mongoose.createConnection(`mongodb://127.0.0.1:27017/importo`).on("open", ()=>{
-    console.log("Mongo Connected")
-}).on("error", ()=>{
-    console.log("Error")
-});
+console.log("Mongo Connecting");
 
-module.exports= connection;
+    // connection = mongoose.createConnection(`mongodb://127.0.0.1:27017/importo`).on("open", ()=>{
+    //     console.log("Mongo Connected")
+    // }).on("error", ()=>{
+    //     console.log("Error")
+    // });
+   const  connection = mongoose.createConnection(`mongodb+srv://root:root@cluster0.btslc8f.mongodb.net`).on("open", ()=>{
+        console.log("Mongo Connected")
+    }).on("error", ()=>{
+        console.log("Error")
+    });
+
+
+module.exports = connection;
--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const db = require('./config/config')
 require('dotenv').config();
 const cors = require('cors');
 
+
+
 const port = process.env.PORT || 80;
 
 app.listen(port,()=>{
-    console.log(`Server Listening on Port http://localhost:${port}`);
+    console.log(`Server Listening on Port ${port}`);
 })
